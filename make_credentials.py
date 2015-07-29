56f88e5f0e460b38ef8a0a9e937a51bdf8c31a82
mursts <mursts@gmail.com>
2015-07-30 00:20:20 +0900

    credentials作成スクリプトを追加

---
+++ make_credentials.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import oauth2client
+from gmusicapi import Musicmanager
+
+CREDENTIALS_PATH = os.path.join(os.path.dirname(__file__),
+                                'credentials.dat')
+
+
+def main():
+    storage = oauth2client.file.Storage(CREDENTIALS_PATH)
+    credentials = storage.get()
+
+    if not credentials or credentials.invalid:
+        Musicmanager.perform_oauth(CREDENTIALS_PATH, open_browser=False)
+
+
+if __name__ == '__main__':
+    main()
